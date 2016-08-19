c76b9fe0e8d2a4965ccb9298837a74c9422e3c0d
bredahe <henrique.breda@itau-unibanco.com.br>
2016-08-19 00:56:22 -0300

    Finalizado projeto

--- app/src/main/java/bredahe/calculadora/MainActivity.java
+++ app/src/main/java/bredahe/calculadora/MainActivity.java
@@ -1,11 +1,17 @@
 package bredahe.calculadora;
 
+import android.content.Context;
+import android.inputmethodservice.InputMethodService;
+import android.inputmethodservice.Keyboard;
+import android.inputmethodservice.KeyboardView;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
@@ -72,32 +78,49 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void clickValidar(View view) {
+        String msgValidacao = "";
         final EditText campoA = (EditText) findViewById(R.id.valor_a);
         final EditText campoB = (EditText) findViewById(R.id.valor_b);
         final EditText campoC = (EditText) findViewById(R.id.valor_c);
-        String msgValidacao = "";
-        // Verfica se existe STRING nos campos
-        if (campoA.getText().toString().matches("\\w") || campoB.getText().toString().matches("\\w") || campoC.getText().toString().matches("\\w")) {
+        final TextView campoResultado = (TextView) findViewById(R.id.textResult);
+        // Verfica se os campos apresentam valores corretos
+        if (campoA.getText().toString().matches("\\D") ||
+                campoA.getText().toString().isEmpty() ||
+                campoB.getText().toString().matches("\\D") ||
+                campoB.getText().toString().isEmpty() ||
+                campoC.getText().toString().matches("\\D") ||
+                campoC.getText().toString().isEmpty()) {
             msgValidacao = "Somente digitos números inteiros e maiores que '0' são aceitos";
+        } else {
+            // Verifica o tipo do triãngulo
+            int valorA = Integer.parseInt(campoA.getText().toString());
+            int valorB = Integer.parseInt(campoB.getText().toString());
+            int valorC = Integer.parseInt(campoC.getText().toString());
+
+            if (valorA == valorB && valorB == valorC) {
+                msgValidacao = "Triângulo Equilátero!\n3 lados iguais.";
+            } else if (valorA == valorB || valorA == valorC || valorB == valorC) {
+                msgValidacao = "Triângulo Isósceles!\n2 lados iguais.";
+            } else {
+                msgValidacao = "Triângulo Escaleno!\nNenhum lado igual.";
+            }
         }
-        // Verifica se existe CARACTERES ESPECIAIS nos campos
-        if (campoA.getText().toString().matches("\\D") || campoB.getText().toString().matches("\\D") || campoC.getText().toString().matches("\\D")) {
-            msgValidacao = "Somente digitos números inteiros e maiores que '0' são aceitos";
-        }
-        // OK
-        if (msgValidacao == ""){
-            msgValidacao = "OK";
-        }
-
+        campoResultado.setText(msgValidacao);
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(campoResultado.getWindowToken(), 0);
     }
 
-    public void clickValorA(View view) {
+
+    public void clickLimpar(View view) {
+        final EditText campoA = (EditText) findViewById(R.id.valor_a);
+        final EditText campoB = (EditText) findViewById(R.id.valor_b);
+        final EditText campoC = (EditText) findViewById(R.id.valor_c);
+        final TextView campoResultado = (TextView) findViewById(R.id.textResult);
+        campoA.setText("");
+        campoB.setText("");
+        campoC.setText("");
+        campoResultado.setText("");
     }
 
-    public void clickValorB(View view) {
-    }
-
-    public void clickValorC(View view) {
-    }
 
 }
